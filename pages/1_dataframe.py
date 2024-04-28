338195f1b146d4cef3c04de2b485c282ab9f9768
keinez56 <49703138+keinez56@users.noreply.github.com>
2024-04-28 14:36:29 +0800

    2 commit

--- pages/1_dataframe.py
+++ pages/1_dataframe.py
@@ -1,5 +1,5 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.read_excel("pages/8.xlsx")
+df = pd.read_excel("https://raw.githubusercontent.com/keinez56/streamlit/main/pages/8.xlsx")
 st.dataframe(df)
